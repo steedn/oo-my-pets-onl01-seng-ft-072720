571677d1379eef79d1a242e3987a1b69754c9110
Nathan Steed <steedn@outlook.com>
2020-08-13 11:52:42 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,21 +1,21 @@
-require "pry"
-
-class Cat
-  @@all = []
-  attr_reader :name
-  attr_accessor :owner, :mood, :cats
-  def initialize(name, owner)
-    @name = name
-    @owner = owner
-    @mood = "nervous"
-    @owner.cats << self
-    save
-    # binding.pry
-  end
-  def save
-    @@all << self
-  end
-  def self.all
-    @@all
-  end
-end
+# require "pry"
+# 
+# class Cat
+#   @@all = []
+#   attr_reader :name
+#   attr_accessor :owner, :mood, :cats
+#   def initialize(name, owner)
+#     @name = name
+#     @owner = owner
+#     @mood = "nervous"
+#     @owner.cats << self
+#     save
+#     # binding.pry
+#   end
+#   def save
+#     @@all << self
+#   end
+#   def self.all
+#     @@all
+#   end
+# end
