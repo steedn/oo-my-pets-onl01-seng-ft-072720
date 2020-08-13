3e8bf55bd0828e5239fa6777af888a7a0c419ace
Nathan Steed <steedn@outlook.com>
2020-08-13 11:55:49 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,38 +1,38 @@
-# require "pry"
-#
-# class Cat
-#   @@all = []
-#   attr_reader :name
-#   attr_accessor :owner, :mood, :cats
-#   def initialize(name, owner)
-#     @name = name
-#     @owner = owner
-#     @mood = "nervous"
-#     @owner.cats << self
-#     save
-#     # binding.pry
-#   end
-#   def save
-#     @@all << self
-#   end
-#   def self.all
-#     @@all
-#   end
-# end
+require "pry"
+
+class Cat
+  @@all = []
+  attr_reader :name
+  attr_accessor :owner, :mood, :cats
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+    @owner.cats << self
+    save
+    # binding.pry
+  end
+  def save
+    @@all << self
+  end
+  def self.all
+    @@all
+  end
+end
 # class Cat
 #   # code goes here
 #   attr_accessor :mood, :owner
 #   attr_reader :name
-# 
+#
 #   @@all = []
-# 
+#
 #   def initialize(name, owner)
 #     @name = name
 #     @mood = "nervous"
 #     @owner = owner
 #     @@all << self
 #   end
-# 
+#
 #   def self.all
 #     @@all
 #   end
