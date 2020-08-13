746a9f1c785eb312a4604966921e1adab0767ec5
Nathan Steed <steedn@outlook.com>
2020-08-13 11:55:45 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -19,21 +19,21 @@
 #     @@all
 #   end
 # end
-class Cat
-  # code goes here
-  attr_accessor :mood, :owner
-  attr_reader :name
-
-  @@all = []
-
-  def initialize(name, owner)
-    @name = name
-    @mood = "nervous"
-    @owner = owner
-    @@all << self
-  end
-
-  def self.all
-    @@all
-  end
-end
+# class Cat
+#   # code goes here
+#   attr_accessor :mood, :owner
+#   attr_reader :name
+# 
+#   @@all = []
+# 
+#   def initialize(name, owner)
+#     @name = name
+#     @mood = "nervous"
+#     @owner = owner
+#     @@all << self
+#   end
+# 
+#   def self.all
+#     @@all
+#   end
+# end
