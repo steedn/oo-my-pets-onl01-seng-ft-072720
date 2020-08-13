369c9a8df91c6777d7bd55fb190127205c7cc088
Nathan Steed <steedn@outlook.com>
2020-08-13 11:55:04 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,12 @@
 class Dog
   @@all = []
   attr_reader :name
-  attr_accessor :owner, :mood, :dogs
+  attr_accessor :owner, :mood
   def initialize(name, owner)
     @name = name
     @owner = owner
     @mood = "nervous"
-    @owner.dogs << self
+    # @owner.dogs << self
     save
   end
   def save
